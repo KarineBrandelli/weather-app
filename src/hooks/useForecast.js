e313297432724aecb7abb0a064a4a0e3e90584ac
Karine Brandelli <karinebrandelli@live.com>
2022-12-15 16:49:16 -0300

    build: create hooks for error, loading and forecast

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,5 +1,9 @@
-const useForecast = () => {
+import { useState } from "react";
 
+const useForecast = () => {
+  const [isError, setError] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [forecast, setForecast] = useState(false);
 };
 
 export default useForecast;
