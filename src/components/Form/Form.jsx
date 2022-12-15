6d01f4482850cd7a4b07064be56b88646d58c034
Karine Brandelli <karinebrandelli@live.com>
2022-12-15 16:33:25 -0300

    build: add useState, create location and setLocation()

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Form.module.css";
 
 const Form = () => {
+  const [location, setLocation] = useState('');
   const onSubmit = (e) => {
     e.preventDefault();
   };
