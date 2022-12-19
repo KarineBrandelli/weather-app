064e180059edaa62cc73af81db8a0cb611aea2b8
Karine Brandelli <karinebrandelli@live.com>
2022-12-19 18:19:00 -0300

    build: add error when location not found

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -6,24 +6,30 @@ const useForecast = () => {
   const [isLoading, setLoading] = useState(false);
   const [forecast, setForecast] = useState(null);
 
-  const submitRequest = async location => {
-    const { data } = await axios(`http://api.weatherapi.com/v1/forecast.json?key=e28f5ea34d47486a80b194043221912&q=${ location }&days=7&aqi=no&alerts=no`);
+  const submitRequest = async (location) => {
+    try {
+      const response = await axios(`http://api.weatherapi.com/v1/forecast.json?key=e28f5ea34d47486a80b194043221912&q=${location}&days=7&aqi=no&alerts=no`);
 
-    console.log({ data });
-    console.log({ location });
+      console.log({ response });
+      console.log({ location });
 
-    if (!data || data.length === 0) {
-      setError('There is no such location');
-      return;
-    };
+      const newResponse = `${response.data.location.lat} ${response.data.location.lon}`;
+      console.log(newResponse);
+
+      if (response.erro) {
+        throw Error("There is no such location");
+      }
+    } catch (erro) {
+      setError("There is no such location");
+    }
   };
 
   return {
     isError,
     isLoading,
     forecast,
-    submitRequest
-  }
+    submitRequest,
+  };
 };
 
 export default useForecast;
