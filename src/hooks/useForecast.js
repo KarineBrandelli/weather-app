c80716f9653a7ea18e900eabbce46bbfe9ac5a0b
Karine Brandelli <karinebrandelli@live.com>
2022-12-19 18:54:35 -0300

    refactor: separate call the api from newResponse

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -6,15 +6,12 @@ const useForecast = () => {
   const [isLoading, setLoading] = useState(false);
   const [forecast, setForecast] = useState(null);
 
-  const submitRequest = async (location) => {
+  const getLocation = async (location) => {
     try {
       const response = await axios(`http://api.weatherapi.com/v1/forecast.json?key=e28f5ea34d47486a80b194043221912&q=${location}&days=7&aqi=no&alerts=no`);
 
       console.log({ response });
       console.log({ location });
-
-      const newResponse = `${response.data.location.lat} ${response.data.location.lon}`;
-      console.log(newResponse);
 
       if (response.erro) {
         throw Error("There is no such location");
@@ -22,6 +19,13 @@ const useForecast = () => {
     } catch (erro) {
       setError("There is no such location");
     }
+  }
+
+  const submitRequest = async (location) => {
+
+
+    const newResponse = await axios(`${response.data.location.lat} ${response.data.location.lon}`);
+    console.log(newResponse);
   };
 
   return {
