8d9564e739093f58a280ed251d2c33a15a76048c
Karine Brandelli <karinebrandelli@live.com>
2022-12-19 17:30:00 -0300

    build: set error for when there's no location

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 const useForecast = () => {
   const [isError, setError] = useState(false);
@@ -6,11 +7,15 @@ const useForecast = () => {
   const [forecast, setForecast] = useState(null);
 
   const submitRequest = async location => {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e28f5ea34d47486a80b194043221912&q=${ location }&days=7&aqi=no&alerts=no`);
-    const responseConvert = await response.json();
-    
-    console.log({ responseConvert });
+    const { data } = await axios(`http://api.weatherapi.com/v1/forecast.json?key=e28f5ea34d47486a80b194043221912&q=${ location }&days=7&aqi=no&alerts=no`);
+
+    console.log({ data });
     console.log({ location });
+
+    if (!data || data.length === 0) {
+      setError('There is no such location');
+      return;
+    };
   };
 
   return {
