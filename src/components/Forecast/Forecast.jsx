05cb5385bc615d4a559251bfca24f2c39a37194c
Karine Brandelli <karinebrandelli@live.com>
2022-12-22 15:42:14 -0300

    build: add current day descripiton information to function

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -9,7 +9,7 @@ import UpcomingDaysForecast from '../UpcomingDaysForecast';
 
 import styles from "./Forecast.module.css";
 
-const Forecast = ({forecast}) => (
+const Forecast = ({ forecast }) => (
   <Container className={styles.box}>
     <Row>
       <Col xs={12} md={4}>
@@ -21,6 +21,7 @@ const Forecast = ({forecast}) => (
         xs={12}
         md={8}
         className="d-flex flex-column justify-content-between" >
+          <CurrentDayDescription forecast={ forecast.currentDayDetails } />
       </Col>
     </Row>
   </Container>
