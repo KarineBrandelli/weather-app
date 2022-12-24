503f134181052091a16e7515e1f333d13f9565c3
Karine Brandelli <karinebrandelli@live.com>
2022-12-24 17:10:48 -0300

    build: add readme file description

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Descrição ✏️
 
 <p align="justify">
-
+A aplicação representa uma página de consulta a previsão do tempo, onde o usuário preenche no campo de pesquisa a cidade que deseja saber a previsão do tempo do dia atual e também para os próximos dois dias. Foi utilizada para a aplicação a <a href="https://www.weatherapi.com/">WeatherAPI</a>, que fornece acesso gratuito a dados meteorológicos e geográficos por meio de uma API restful JSON.
 </p>
 
 <hr>
--- src/helpers/getUpcomingDaysForecast.js
+++ src/helpers/getUpcomingDaysForecast.js
@@ -9,12 +9,46 @@ let weekdays = [
 ];
 
 let currentDate = new Date();
+let weekday = currentDate.getDay()
+
 let firstweekday = weekdays[(currentDate).getDay() + 1];
 let secondweekday = weekdays[(currentDate).getDay() + 2];
 
-if (firstweekday === 'Saturday') {
-  secondweekday = weekdays[0]
+
+// if (firstweekday === 'Saturday') {
+//   secondweekday = weekdays[0]
+// }
+
+
+
+for (let i = 0; i < weekdays.length; i++) {
+  if (i = 0) {
+    firstweekday = "Monday";
+    secondweekday = "Tuesday";
+  } else if (i = 1) {
+    firstweekday = "Tuesday";
+    secondweekday = "Wednesday";
+  } else if (i = 2) {
+    firstweekday = "Wednesday";
+    secondweekday = "Thursday";
+  } else if (i = 3) {
+    firstweekday = "Thursday";
+    secondweekday = "Friday";
+  } else if (i = 4) {
+    firstweekday = "Friday";
+    secondweekday = "Saturday";
+  } else if (i = 5) {
+    firstweekday = "Saturday";
+    secondweekday = "Sunday";
+  } else if (i = 6) {
+    firstweekday = "Sunday";
+    secondweekday = "Monday";
+  }
 };
+
+// if (firstweekday === 'Sunday') {
+//   secondweekday = weekend
+// }
 
 const getUpcomingDaysForecast = (data) => ({
   nextdaytemperature: Math.round(data.forecast.forecastday[1].day.maxtemp_c),
